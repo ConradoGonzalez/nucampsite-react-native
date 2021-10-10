997092572403e55ccbedcadf4abdb9ac3293c6d2
ConradoGonzalez <86978884+ConradoGonzalez@users.noreply.github.com>
2021-10-10 10:31:39 -0500

    Week 2 Workshop

--- redux/comments.js
+++ redux/comments.js
@@ -1,12 +1,17 @@
 import * as ActionTypes from './ActionTypes';
 
-export const comments = (state = { errMess: null, comments: []}, action) => {
+export const comments = (state = { errMess: null, comments: [] }, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
-            return {...state, errMess: null, comments: action.payload};
+            return { ...state, errMess: null, comments: action.payload };
+
+        case ActionTypes.ADD_COMMENT:
+            const comment = { ...action.payload, id: state.comments.length }
+            const comments = [...state.comments, comment]
+            return { ...state, errMess: null, comments };
 
         case ActionTypes.COMMENTS_FAILED:
-            return {...state, errMess: action.payload};
+            return { ...state, errMess: action.payload };
 
         default:
             return state;
